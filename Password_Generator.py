0de3384413bc27b4cd2c0e3bc09812ac1ad39262
MartynaWalczak <57598900+MartynaWalczak@users.noreply.github.com>
2019-11-13 12:02:37 +0100

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -7,15 +7,15 @@ if __name__ == '__main__':
 #Password Generator
 import random
 def password(power,long):
-    if power == 1 or 'weak':
-        haslo_w = [chr(i) for i in range(97,122)] #create list [set chr(from i) when i is in order numb from range: 97 to 121]; chr translator from ascii to text
-        haslo_w = ''.join(str(x) for x in (random.sample(haslo_w,long)))
-        print("Your weak password is: ", haslo_w)
-    elif power == 2 or 'medium':
+    if power == 1:
+        passw_w = [chr(i) for i in range(97,122)] #create list [set chr(from i) when i is in order numb from range: 97 to 121]; chr translator from ascii to text
+        passw_w = ''.join(str(x) for x in (random.sample(passw_w,long))) #join with '' empty gap (str(from x) when x is in order (random choise(from pazzw_w, population = long)
+        print("Your weak password is: ", passw_w)
+    elif power == 2:
         haslo_m = [chr(i) for i in range(65,91)] + [chr(i) for i in range(97,122)]
         haslo_m = ''.join(str(x) for x in (random.sample(haslo_m,long)))
         print("Your medium password is: ", haslo_m)
-    elif power == 3 or 'strong':
+    elif power == 3:
         haslo_s = [chr(i) for i in range(65,91)] + [chr(i) for i in range(97,122)] + list(range(0,11)) + ["_", "-"]
         haslo_s = ''.join(str(x) for x in (random.sample(haslo_s,long)))
         print("Your strong password is: ", haslo_s)
