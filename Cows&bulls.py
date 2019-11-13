6e8e9f359f75da08dc082bd0f120b0d3ee9beb91
MartynaWalczak <57598900+MartynaWalczak@users.noreply.github.com>
2019-11-13 12:10:44 +0100

    Update Cows&bulls.py

--- Cows&bulls.py
+++ Cows&bulls.py
@@ -7,23 +7,21 @@ if __name__ == '__main__':
 #Create a program that will play the “cows and bulls” game with the user.
 import random
 def c_b():
-    numb = random.sample(range(0,10),4)
-    print(numb)
+    numb = random.sample(range(10),4) #creat random choise number from(range 0 to 10, population = 4)
     while True:
         guess = input("Your guess: ")
         user = []
-        for i in guess:
-            user.append(int(i))
+        for i in guess: #i is in order number from ouer guess
+            user.append(int(i)) #add every i to list "user"
             cow = 0
 
-        for i in range(0,4):
-            for j in range(0,4):
-                if numb[i] == user[i]:
-                    cow += 1
+        for i in range(4): #i is in order range 0 to 3
+            if numb[i] == user[i]: #if numb[i pozition in list] = user[i pozition in list]
+                cow += 1
 
         bull = 0
-        for i in range(0,4):
-            for j in range(0,4):
+        for i in range(4):
+            for j in range(4):
                 if numb[i] == user [j]:
                     bull += 1
 
