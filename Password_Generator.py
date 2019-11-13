3e94a36d237b76664f201fc00f0b411814f4ae58
MartynaWalczak <57598900+MartynaWalczak@users.noreply.github.com>
2019-11-13 11:09:37 +0100

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Georgina
-#
-# Created:     13.11.2019
-# Copyright:   (c) Georgina 2019
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 def main():
     pass
 
@@ -21,13 +10,14 @@ def password():
     power = int(input("How strong do you want the password? (1.weak, 2.medium, 3.strong): "))
     long = int(input("How long do you want the password? "))
     if power == 1:
-       haslo_w = [chr(i) for i in range(97,122)]
+        haslo_w = [chr(i) for i in range(97,122)]
         haslo_w = ''.join(str(x) for x in (random.sample(haslo_w,long)))
         print("Your weak password is: ", haslo_w)
     elif power == 2:
         haslo_m = [chr(i) for i in range(65,91)] + [chr(i) for i in range(97,122)]
         haslo_m = ''.join(str(x) for x in (random.sample(haslo_m,long)))
-   elif power == 3:
+        print("Your weak password is: ", haslo_m)
+    elif power == 3:
         haslo_s = [chr(i) for i in range(65,91)] + [chr(i) for i in range(97,122)] + list(range(0,11)) + ["_", "-"]
         haslo_s = ''.join(str(x) for x in (random.sample(haslo_s,long)))
         print("Your strong password is: ", haslo_s)
